485953adf0473109eaa2aec287082c48998813bd
Saumya Patel <patelsaumya767@gmail.com>
2022-02-26 23:02:15 +0400

    format code

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,5 @@
 # imports
-import pyautogui as pg 
+import pyautogui as pg
 import time
 
 # initialize grid
@@ -22,28 +22,30 @@ while True:
 # give the user a few seconds to click on the initial square
 time.sleep(10)
 
+
 def possible(x, y, n):
     """
     checks if the number n is possible in the given row, column, and 3x3 square
     """
     # check row
     for i in range(0, 9):
-        if grid[i][x] == n and i != y: 
+        if grid[i][x] == n and i != y:
             return False
 
     # check column
     for i in range(0, 9):
-        if grid[y][i] == n and i != x: 
+        if grid[y][i] == n and i != x:
             return False
 
     # check 3x3 square
     x0 = (x // 3) * 3
     y0 = (y // 3) * 3
     for X in range(x0, x0 + 3):
-        for Y in range(y0, y0 + 3):  
+        for Y in range(y0, y0 + 3):
             if grid[Y][X] == n:
-                return False    
+                return False
     return True
+
 
 def fill(matrix):
     """
@@ -64,11 +66,10 @@ def fill(matrix):
         pg.press(num)
         pg.hotkey("right")
         counter.append(num)
-        if len(counter)%9 == 0:
+        if len(counter) % 9 == 0:
             pg.hotkey("down")
             for _ in range(8):
                 pg.hotkey("left")
-
 
 
 def solve():
@@ -87,6 +88,7 @@ def solve():
                 return
     fill(grid)
     input("Solved")
+
 
 if __name__ == "__main__":
     # solve the sudoku
